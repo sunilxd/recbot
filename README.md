015262c668ce1a9dbe2dc8b71e61d454ba822f84
sunilkumar <sunil@pop-os.localdomain>
2022-12-13 21:04:14 +0530

    Bot rebranding

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tinson
+
+* Telegram bot to get your mark detail from [rajalakshmi.in](http://rajalakshmi.in/)
+* easy to use
+* @tinson_bot
--- main.py
+++ main.py
@@ -1,159 +1,191 @@
-from dotenv import load_dotenv
-import os
-import telebot
-from telebot import types
-from database import *
+import logging
 import pickle
+from unified import *
+from prettytable import PrettyTable
 
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
-PIN = os.getenv('PIN')
-bot = telebot.TeleBot(API_KEY)
 
-chat_manager = {}
-table = data_to_dic()
-timetable = pickle.load(open('timetable.pickle', 'rb'))
-days = ['tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, ParseMode
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 
-def get_timetable(day):
-    global timetable
-    per = timetable[day]
-    tim = timetable[timetable['tim'][day]]
-    te = ''
-    for i in range(len(per)):
-        te += tim[i]+'\n'
-        te += timetable['map'][timetable[day][i]]+'\n\n'
-    return te
+with open('stu_data.pkl', 'rb') as f:
+    student_record = pickle.load(f)
 
-def get_marks(column):
-    i = int(get_data('Data', 'f1')[0][0])
-    mar = [ele[0] for ele in get_data('Mark', f'{column}2:{column}{i}')]
-    return mar
 
-help_message = """If you're new, please see the [manual](https://happi.sunilkumar70.repl.co/).
 
-*You can control me by sending these commands:*
 
-*Student*
-/join - enroll yourself (only for AIML)
-/timetable - for today timetable
-/leave - tell your teacher your are going to be absent today
+# Enable logging
+logging.basicConfig(level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s")
 
-*Teacher*
-/mark - send marks of the students
-/leave - get list of students who are absent
+logger = logging.getLogger(__name__)
 
-*Others*
-/cancel - cancel the operation
-/help - to know about the commands
-"""
+ROLLNO, SEM, EXAM = range(3)
 
-@bot.message_handler(func=lambda m: True)
-def tinson(message):
-    global table
-    sender_id = message.chat.id
-    msg = message.text
-    # main commands
-    if msg=='/join':
-        if sender_id in table:
-            chat_manager[sender_id] = {'msg':'join_roll'}
-            bot.send_message(sender_id, 'You already joined\nroll no {roll}\nname {name}\nTo modify type your roll no\nuse /cancel to dismiss'.\
-                format(roll = table[sender_id]['roll'], name = table[sender_id]['name']))
-        else:
-            chat_manager[sender_id] = {'msg':'join_pin'}
-            bot.send_message(sender_id, 'Alright, Enter a pin to continue\n(only for AIML student)')
-    elif msg=='/timetable':
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        itembtna = types.KeyboardButton('Tuesday')
-        itembtnv = types.KeyboardButton('Wednesday')
-        itembtnc = types.KeyboardButton('Thursday')
-        itembtnd = types.KeyboardButton('Friday')
-        itembtne = types.KeyboardButton('Saturday')
-        markup.row(itembtna, itembtnv)
-        markup.row(itembtnc, itembtnd, itembtne)
-        bot.send_message(sender_id, "Choose one day:", reply_markup=markup)
-    elif msg=='/leave':
-        if sender_id in table and table[sender_id]['role']:
-            lis = ['{} {}'.format(table[ele]['roll'], table[ele]['name']) for ele in table if table[ele]['leave']==1]
-            te = 'List:\n'+'\n'.join(lis)
-            te = 'No absentes' if te=='List:\n' else te
-            bot.send_message(sender_id, te)
-        elif sender_id in table:
-            if table[sender_id]['leave']==1:
-                table[sender_id]['leave'] = 0
-                te = 'Marked as present'
-            else:
-                table[sender_id]['leave'] = 1
-                te = 'Marked as absent'
-            dic_to_data(table)
-            bot.send_message(sender_id, te) 
-        else:
-            bot.send_message(sender_id, 'You are not authorized\nPlease use /join to add your details')
-    elif msg=='/mark':
-        if table[sender_id]['role']:
-            chat_manager[sender_id] = {'msg':'mark_column'}
-            bot.send_message(sender_id, 'Enter the column which contain marks:')
-        else:
-            bot.send_message('This feature is only for teachers(beta)')
+def start(update: Update, context: CallbackContext) -> int:
+    """Starts the conversation and asks the user about their rollno"""
 
-    elif msg=='/refresh' and table[sender_id]['role']:
-        table = data_to_dic()
-        bot.send_message(sender_id, 'table refreshed')
-    elif msg=='/reset' and table[sender_id]['role']:
-        i = int(get_data('Data', 'f1')[0][0])
-        data = [[0] for j in range(i-1)]
-        update_data_to(data, 'Data', 'e2')
-        bot.send_message(sender_id, 'Reset done.')
-    elif msg=='/cancel':
-        chat_manager[sender_id] = {'msg':'None'}
-        bot.send_message(sender_id, 'operation canceled')
-    elif msg=='/start' or msg=='/help':
-        bot.send_message(sender_id, help_message, parse_mode='Markdown')
+    update.message.reply_text(
+        "Hi! My name is REC Bot.\n"
+        "I know unified portal is a crap\n"
+        "I can fetch your marks in seconds\n\n"
+        "Enter your roll number"
+    )
 
-    # all join operations
-    elif sender_id in chat_manager and chat_manager[sender_id]['msg']=='join_pin':
-        if int(msg)==int(PIN):
-            chat_manager[sender_id]['msg'] = 'join_roll'
-            bot.send_message(sender_id, 'Verified!\nwhat is your roll number?')
-        else:
-            chat_manager[sender_id] = {'msg':'None'}
-            bot.send_message(sender_id, 'sorry wrong pin')
-    elif sender_id in chat_manager and chat_manager[sender_id]['msg']=='join_roll':
-        chat_manager[sender_id]['msg'] = 'join_name'
-        chat_manager[sender_id]['roll'] = msg
-        bot.send_message(sender_id, 'what is your name?')
-    elif sender_id in chat_manager and chat_manager[sender_id]['msg']=='join_name':
-        chat_manager[sender_id]['msg'] = 'None'
-        table[sender_id] = {'name':msg, 'roll':chat_manager[sender_id]['roll'], 'role':False, 'leave':0}
-        dic_to_data(table)
-        bot.send_message(sender_id, 'Your details added')
-    
-    # mark operation
-    elif sender_id in chat_manager and chat_manager[sender_id]['msg']=='mark_column':
-        chat_manager[sender_id]['msg'] = 'mark_text'
-        chat_manager[sender_id]['col'] = msg
-        bot.send_message(sender_id, 'Exam name?')
-    elif sender_id in chat_manager and chat_manager[sender_id]['msg']=='mark_text':
-        table = data_to_dic()
-        chat_manager[sender_id]['msg'] = 'None'
-        mar = get_marks(chat_manager[sender_id]['col'])
-        chat_ids = list(table.keys())
-        for ele in table:
-            if not (table[ele]['role']):
-                bot.send_message(ele, f'{msg}\n{mar[chat_ids.index(ele)]}')
-        bot.send_message(sender_id, 'Done')
-    
-    # time talbe function
-    elif msg.lower() in days:
-        te = get_timetable(msg.lower()[:3])
-        bot.send_message(sender_id, te)
+    return ROLLNO
 
-    # unknown command
-    elif msg.lower().startswith('/'):
-        bot.send_message(sender_id, 'Unrecognized command. Say what?')
 
-    # send help command
-    else:
-        bot.send_message(sender_id, help_message, parse_mode='Markdown')
+def get_rollno(update: Update, context: CallbackContext) -> int:
+    """"Get the roll number and check if the roll number is valid then ask about semester"""
 
-bot.infinity_polling()
+    userid = update.message.from_user.id
+    user = update.message.from_user.first_name
+    roll_no = update.message.text
+
+    sry_message = "I never seen that rollno in unified portal"
+
+
+    if roll_no not in student_record:
+        update.message.reply_text(sry_message)
+
+        return ConversationHandler.END
+
+    user_id, user_name = student_record[roll_no]
+
+    logger.info(f"{user_id} : {roll_no}({user_name})")
+
+    try:
+        marks = get_internal_filter(user_id)
+
+    except Exception as e:
+        logger.error(e)
+
+        update.message.reply_text(
+            "Hmmmm.... rajalakshmi.in is down\n\n"
+            "Try after sometime")
+
+        return ConversationHandler.END
+
+
+    context.user_data["marks"] = marks
+
+    reply_keyboard = [[semester] for semester in sorted(marks)]
+
+    update.message.reply_text(
+        f"Got you {user_name}\n"
+        "Pick the semester",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True
+        ),
+    )
+
+    return SEM
+
+
+def get_semester(update: Update, context: CallbackContext) -> int:
+    """Get the semester check if it is valid then ask about Exam"""
+
+    exam = update.message.text
+
+    if exam not in context.user_data["marks"]:
+        update.message.reply_text(
+            "That is not a valid semester\n"
+            "Please Try again"
+        )
+
+        return ConversationHandler.END
+
+    context.user_data["marks"] = marks = context.user_data["marks"][exam]
+
+    reply_keyboard = [[exam] for exam in sorted(marks)]
+
+    update.message.reply_text(
+        "What marks do you want?",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True
+        ),
+    )
+
+    return EXAM
+
+
+def beautify_marks(marks):
+    table = PrettyTable(['Subject Name', 'Marks'])
+    table = "\n"
+    # table.align['Subject Name'] = 'l'
+    # table.align['Marks'] = 'r'
+
+    for mark in marks:
+        # table.add_row([mark['subject'], mark['total']])
+        table += f"{mark['subject']}: {mark['total']}\n"
+
+    return table
+
+def get_exam(update: Update, context: CallbackContext) -> int:
+    """Get the exam if valid show the marks"""
+
+    exam = update.message.text
+
+    if exam not in context.user_data["marks"]:
+        update.message.reply_text(
+            "Sorry didn't see that exam name in database"
+        )
+
+        return ConversationHandler.END
+
+    marks = context.user_data["marks"][exam]
+
+    table = beautify_marks(marks)
+
+    update.message.reply_text(f"```{table}```", parse_mode=ParseMode.MARKDOWN_V2, reply_markup=ReplyKeyboardRemove(),)
+
+    return ConversationHandler.END
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+
+    update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+def main() -> None:
+    """Run the bot"""
+    updater = Updater("5939179125:AAHpp_kujpnUnMv8hmzjs2BtE-rT7Rm-oeM")
+
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # conversation handler
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            ROLLNO: [MessageHandler(Filters.text, get_rollno)],
+            SEM: [MessageHandler(Filters.text, get_semester)],
+            EXAM: [MessageHandler(Filters.text, get_exam)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+
+    # run
+    updater.start_polling()
+
+
+    updater.idle()
+
+
+
+if __name__ == "__main__":
+    main()
