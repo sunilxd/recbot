7d44837b88f4ecc6d8fff9b33d10b71334993e7a
sunilkumar <sunil@pop-os.localdomain>
2022-12-21 15:04:11 +0530

    added some options

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Tinson
+# REC bot
+@rec_unified_bot
 
 
-## Soon going to change the repo name to REC bot
-
-* Telegram bot to get your mark detail from [rajalakshmi.in](http://rajalakshmi.in/)
 * easy to use
-* @rec_unified_bot
+* Telegram bot to get your mark detail from [rajalakshmi.in](http://rajalakshmi.in/)
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
+import os
 import pickle
 from unified import *
-from prettytable import PrettyTable
 
 
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, ParseMode
@@ -22,31 +22,127 @@ with open('stu_data.pkl', 'rb') as f:
 
 # Enable logging
 logging.basicConfig(level=logging.INFO,
+    filename='info.log',
+    format="%(asctime)s - %(levelname)s - %(message)s")
+
+logging.basicConfig(level=logging.ERROR, 
+    filename='error.log',
     format="%(asctime)s - %(levelname)s - %(message)s")
 
 logger = logging.getLogger(__name__)
 
-ROLLNO, SEM, EXAM = range(3)
+FEEDBACK, ROLLNO, SEM, EXAM = range(4)
 
-def start(update: Update, context: CallbackContext) -> int:
-    """Starts the conversation and asks the user about their rollno"""
+def start(update: Update, context: CallbackContext) -> None:
+    """Default message"""
 
     update.message.reply_text(
-        "Hi! My name is REC Bot.\n"
+        "Hi! I am REC bot\n"
         "I know unified portal is a crap\n"
         "I can fetch your marks in seconds\n\n"
-        "Enter your roll number"
+        "/mymark - to get your mark\n"
+        "/othersmark - to get others mark\n"
+        "/change - to change your rollno\n"
+        "/feedback - like to change me? give a feedback\n"
+        "/about - about this project"
+    )
+
+    return None
+
+def mymark(update: Update, context: CallbackContext) -> int:
+    """Ask about the roll number"""
+    context.user_data['own'] = True
+
+    if 'roll' in context.user_data:
+        return get_rollno(update, context)
+
+    update.message.reply_text(
+        "Roll number please\n"
     )
 
     return ROLLNO
 
 
+def othersmark(update: Update, context: CallbackContext) -> int:
+    """Ask about the roll number"""
+    context.user_data['own'] = False
+
+    update.message.reply_text(
+        "Roll number please\n"
+    )
+
+    return ROLLNO
+
+def changeroll(update: Update, context: CallbackContext) -> int:
+    """Change the default roll number"""
+    context.user_data['own'] = True
+
+    if 'roll' in context.user_data:
+        del context.user_data['roll']
+
+    update.message.reply_text(
+        "Roll number please\n"
+    )
+
+    return ROLLNO
+
+
+def about(update: Update, context: CallbackContext) -> int:
+    """Send a about message"""
+
+
+    update.message.reply_text(
+        "Filtering the cat marks in the unified portal is a nightmare\n"
+        "so made this simple bot to save your time \n\n"
+        "Here is the [github link](https://github.com/sunilxd/recbot)\n\n\n"
+        "\\~\n"
+        "telegram [DM](tg://user?id=1156186009)\n"
+        "Insagram [sunilkumar](https://www.instagram.com/sunilkumar.0_o)\n"
+        "GitHub [sunilxd](https://github.com/sunilxd)",
+        parse_mode=ParseMode.MARKDOWN_V2, disable_web_page_preview=True
+    )
+
+
+def feedbackstart(update: Update, context: CallbackContext) -> int:
+    """ask for a feedback"""
+    update.message.reply_text(
+        "Leave your feedback as a message"
+    )
+
+    return FEEDBACK
+
+def feedback(update: Update, context: CallbackContext) -> int:
+    """get the feedback and send to me"""
+    msg = update.message.text
+    logger.info(f'feedback - {msg}')
+
+    # send feedback to sunil 1156186009
+    msg = f"{update.message.from_user.id} - {update.message.from_user.full_name}\nfeedback\n{msg}"
+    context.bot.send_message(text=msg, chat_id=1156186009)
+
+
+
+    update.message.reply_text(
+        "I send that to the developer\n"
+        "Can't wait?\n"
+        "You can give a pull request [here](https://github.com/sunilxd/recbot)",
+        parse_mode=ParseMode.MARKDOWN_V2,
+        disable_web_page_preview=True,
+    )
+
+    return ConversationHandler.END
+
 def get_rollno(update: Update, context: CallbackContext) -> int:
     """"Get the roll number and check if the roll number is valid then ask about semester"""
 
     userid = update.message.from_user.id
-    user = update.message.from_user.first_name
-    roll_no = update.message.text
+    user = update.message.from_user.full_name
+
+    if context.user_data['own'] and ('roll' in context.user_data):
+        roll_no = context.user_data['roll']
+
+    else:
+        roll_no = update.message.text
 
     sry_message = "I never seen that rollno in unified portal"
 
@@ -58,7 +154,12 @@ def get_rollno(update: Update, context: CallbackContext) -> int:
 
     user_id, user_name = student_record[roll_no]
 
-    logger.info(f"{user_id} : {roll_no}({user_name})")
+    if context.user_data['own']:
+        context.user_data['roll'] = roll_no
+
+    
+    logger.info(f"{userid}({user}) : {roll_no}({user_name})")
+    print(f"{userid}({user}) : {roll_no}({user_name})")
 
     try:
         marks = get_internal_filter(user_id)
@@ -116,19 +217,15 @@ def get_semester(update: Update, context: CallbackContext) -> int:
 
 
 def beautify_marks(marks):
-    table = PrettyTable(['Subject Name', 'Marks'])
     table = "\n"
-    # table.align['Subject Name'] = 'l'
-    # table.align['Marks'] = 'r'
 
     for mark in marks:
-        # table.add_row([mark['subject'], mark['total']])
         table += f"{mark['subject']}: {mark['total']}\n"
 
     return table
 
 def get_exam(update: Update, context: CallbackContext) -> int:
-    """Get the exam if valid show the marks"""
+    """Get the exam; if valid show the marks"""
 
     exam = update.message.text
 
@@ -159,7 +256,11 @@ def cancel(update: Update, context: CallbackContext) -> int:
 
 def main() -> None:
     """Run the bot"""
-    updater = Updater("")
+    token = os.getenv('TOKEN')
+    if token == None:
+        token = pickle.load(open('token.pkl', 'rb'))
+
+    updater = Updater(token)
 
 
     # Get the dispatcher to register handlers
@@ -167,8 +268,16 @@ def main() -> None:
 
     # conversation handler
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
+        entry_points=[
+            CommandHandler("mymark", mymark),
+            CommandHandler("othersmark", othersmark),
+            CommandHandler("change", changeroll),
+            CommandHandler("feedback", feedbackstart),
+            CommandHandler("about", about),
+            MessageHandler(Filters.text, start),
+        ],
         states={
+            FEEDBACK: [MessageHandler(Filters.text, feedback)],
             ROLLNO: [MessageHandler(Filters.text, get_rollno)],
             SEM: [MessageHandler(Filters.text, get_semester)],
             EXAM: [MessageHandler(Filters.text, get_exam)],
