ce61caf0c11683219fe5f1dfe57e26fa7ca066c6
sunilxd <sunilxd@github.com>
2023-05-23 18:09:54 +0530

    async

--- main.py
+++ main.py
@@ -2,41 +2,41 @@ import logging
 import os
 import pickle
 from unified import *
-from keep_alive import keep_alive
+from dotenv import load_dotenv
 
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, ParseMode
+load_dotenv()
+
+from telegram.constants import ParseMode
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
-    Updater,
+    Application,
     CommandHandler,
-    MessageHandler,
-    Filters,
+    ContextTypes,
     ConversationHandler,
-    CallbackContext,
+    MessageHandler,
+    filters,
 )
 
-with open('stu_data.pkl', 'rb') as f:
+with open("stu_data.pkl", "rb") as f:
     student_record = pickle.load(f)
 
-
+TEST = True
+LOCAL = True
 
 
 # Enable logging
-logging.basicConfig(level=logging.INFO,
-    filename='info.log',
-    format="%(asctime)s - %(levelname)s - %(message)s")
-
-logging.basicConfig(level=logging.ERROR, 
-    filename='error.log',
-    format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
 FEEDBACK, ROLLNO, SEM, EXAM = range(4)
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Default message"""
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """async default message"""
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "Hi! I am REC bot\n"
         "I know unified portal is a crap\n"
         "I can fetch your marks in seconds\n\n"
@@ -49,49 +49,49 @@ def start(update: Update, context: CallbackContext) -> None:
 
     return None
 
-def mymark(update: Update, context: CallbackContext) -> int:
+async def mymark(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask about the roll number"""
-    context.user_data['own'] = True
+    context.user_data["own"] = True
 
-    if 'roll' in context.user_data:
+    if "roll" in context.user_data:
         return get_rollno(update, context)
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "Roll number please\n"
     )
 
     return ROLLNO
 
 
-def othersmark(update: Update, context: CallbackContext) -> int:
+async def othersmark(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask about the roll number"""
-    context.user_data['own'] = False
+    context.user_data["own"] = False
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "Roll number please\n"
     )
 
     return ROLLNO
 
-def changeroll(update: Update, context: CallbackContext) -> int:
-    """Change the default roll number"""
-    context.user_data['own'] = True
+async def changeroll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Change the async default roll number"""
+    context.user_data["own"] = True
 
-    if 'roll' in context.user_data:
-        del context.user_data['roll']
+    if "roll" in context.user_data:
+        del context.user_data["roll"]
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "Roll number please\n"
     )
 
     return ROLLNO
 
 
-def about(update: Update, context: CallbackContext) -> int:
+async def about(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Send a about message"""
 
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "Filtering the cat marks in the unified portal is a nightmare\n"
         "so made this simple bot to save your time \n\n"
         "Here is the [github link](https://github.com/sunilxd/recbot)\n\n\n"
@@ -103,18 +103,18 @@ def about(update: Update, context: CallbackContext) -> int:
     )
 
 
-def feedbackstart(update: Update, context: CallbackContext) -> int:
+async def feedbackstart(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """ask for a feedback"""
-    update.message.reply_text(
+    await update.message.reply_text(
         "Leave your feedback as a message"
     )
 
     return FEEDBACK
 
-def feedback(update: Update, context: CallbackContext) -> int:
+async def feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """get the feedback and send to me"""
     msg = update.message.text
-    logger.info(f'feedback - {msg}')
+    logger.info(f"feedback - {msg}")
 
     # send feedback to sunil 1156186009
     msg = f"{update.message.from_user.id} - {update.message.from_user.full_name}\nfeedback\n{msg}"
@@ -122,7 +122,7 @@ def feedback(update: Update, context: CallbackContext) -> int:
 
 
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "I send that to the developer\n"
         "Can't wait?\n"
         "You can give a pull request [here](https://github.com/sunilxd/recbot)",
@@ -132,14 +132,14 @@ def feedback(update: Update, context: CallbackContext) -> int:
 
     return ConversationHandler.END
 
-def get_rollno(update: Update, context: CallbackContext) -> int:
+async def get_rollno(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """"Get the roll number and check if the roll number is valid then ask about semester"""
 
     userid = update.message.from_user.id
     user = update.message.from_user.full_name
 
-    if context.user_data['own'] and ('roll' in context.user_data):
-        roll_no = context.user_data['roll']
+    if context.user_data["own"] and ("roll" in context.user_data):
+        roll_no = context.user_data["roll"]
 
     else:
         roll_no = update.message.text
@@ -148,14 +148,14 @@ def get_rollno(update: Update, context: CallbackContext) -> int:
 
 
     if roll_no not in student_record:
-        update.message.reply_text(sry_message)
+        await update.message.reply_text(sry_message)
 
         return ConversationHandler.END
 
     user_id, user_name = student_record[roll_no]
 
-    if context.user_data['own']:
-        context.user_data['roll'] = roll_no
+    if context.user_data["own"]:
+        context.user_data["roll"] = roll_no
 
     
     logger.info(f"{userid}({user}) : {roll_no}({user_name})")
@@ -167,7 +167,7 @@ def get_rollno(update: Update, context: CallbackContext) -> int:
     except Exception as e:
         logger.error(e)
 
-        update.message.reply_text(
+        await update.message.reply_text(
             "Hmmmm.... rajalakshmi.in is down\n\n"
             "Try after sometime")
 
@@ -178,7 +178,7 @@ def get_rollno(update: Update, context: CallbackContext) -> int:
 
     reply_keyboard = [[semester] for semester in sorted(marks)]
 
-    update.message.reply_text(
+    await update.message.reply_text(
         f"Got you {user_name}\n"
         "Pick the semester",
         reply_markup=ReplyKeyboardMarkup(
@@ -189,13 +189,13 @@ def get_rollno(update: Update, context: CallbackContext) -> int:
     return SEM
 
 
-def get_semester(update: Update, context: CallbackContext) -> int:
+async def get_semester(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Get the semester check if it is valid then ask about Exam"""
 
     exam = update.message.text
 
     if exam not in context.user_data["marks"]:
-        update.message.reply_text(
+        await update.message.reply_text(
             "That is not a valid semester\n"
             "Please Try again"
         )
@@ -206,7 +206,7 @@ def get_semester(update: Update, context: CallbackContext) -> int:
 
     reply_keyboard = [[exam] for exam in sorted(marks)]
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "What marks do you want?",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True
@@ -220,17 +220,17 @@ def beautify_marks(marks):
     table = "\n"
 
     for mark in marks:
-        table += f"{mark['subject']}: {mark['total']}\n"
+        table += "{mark}: {total}\n".format(mark = mark["subject"], total = mark["total"])
 
     return table
 
-def get_exam(update: Update, context: CallbackContext) -> int:
+async def get_exam(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Get the exam; if valid show the marks"""
 
     exam = update.message.text
 
     if exam not in context.user_data["marks"]:
-        update.message.reply_text(
+        await update.message.reply_text(
             "Sorry didn't see that exam name in database"
         )
 
@@ -240,15 +240,15 @@ def get_exam(update: Update, context: CallbackContext) -> int:
 
     table = beautify_marks(marks)
 
-    update.message.reply_text(f"```{table}```", parse_mode=ParseMode.MARKDOWN_V2, reply_markup=ReplyKeyboardRemove(),)
+    await update.message.reply_text(f"```{table}```", parse_mode=ParseMode.MARKDOWN_V2, reply_markup=ReplyKeyboardRemove(),)
 
     return ConversationHandler.END
 
 
-def cancel(update: Update, context: CallbackContext) -> int:
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Cancels and ends the conversation."""
 
-    update.message.reply_text(
+    await update.message.reply_text(
         "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
     )
 
@@ -256,19 +256,9 @@ def cancel(update: Update, context: CallbackContext) -> int:
 
 def main() -> None:
     """Run the bot"""
-    token = os.getenv('TOKEN')
-    if token == None:
-        token = pickle.load(open('token.pkl', 'rb'))
+    token = os.getenv("TEST_TOKEN") if TEST else os.getenv("TOKEN")
 
-
-    ## running the flask to keep the repl running
-    flask_thread = keep_alive()
-    
-    updater = Updater(token)
-
-
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    application = Application.builder().token(token).build()
 
     # conversation handler
     conv_handler = ConversationHandler(
@@ -278,25 +268,22 @@ def main() -> None:
             CommandHandler("change", changeroll),
             CommandHandler("feedback", feedbackstart),
             CommandHandler("about", about),
-            MessageHandler(Filters.text, start),
+            MessageHandler(filters.TEXT, start),
         ],
         states={
-            FEEDBACK: [MessageHandler(Filters.text, feedback)],
-            ROLLNO: [MessageHandler(Filters.text, get_rollno)],
-            SEM: [MessageHandler(Filters.text, get_semester)],
-            EXAM: [MessageHandler(Filters.text, get_exam)],
+            FEEDBACK: [MessageHandler(filters.TEXT, feedback)],
+            ROLLNO: [MessageHandler(filters.TEXT, get_rollno)],
+            SEM: [MessageHandler(filters.TEXT, get_semester)],
+            EXAM: [MessageHandler(filters.TEXT, get_exam)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
-    dispatcher.add_handler(conv_handler)
+    application.add_handler(conv_handler)
 
 
     # run
-    updater.start_polling()
-
-
-    updater.idle()
+    application.run_polling()
 
 
 
